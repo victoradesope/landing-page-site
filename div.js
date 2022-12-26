7a9341a45c412d2cc40e6888f6c6549994081350
victoradesope <victoradesope@gmail.com>
2022-12-26 00:13:10 -0800

    landing page

---
+++ div.js
@@ -0,0 +1,8 @@
+var a= 6;
+var b= 7;
+var c= a+b;
+document.write(c);
+const car = ["volvo", "bmw", "toyota"];
+car[0] = "ford";
+car.push("hummer");
+document.getElementById("demo").innerHTML = car;
